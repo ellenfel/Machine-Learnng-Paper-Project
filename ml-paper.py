b0b0e85d2e49086f4e7c987b94af654e1a365edf
ellenfel <ellfelfen@gmail.com>
2022-12-15 18:10:00 +0300

    start building the demo

--- ml-paper.py
+++ ml-paper.py
@@ -1,7 +1,92 @@
 #!/home/ellenfel/anaconda3/envs/smartersnakes
 # -*- coding: utf-8 -*-
 
+#to do : write with object oriented princibiles 
+
+
 #importing relitive libs
 import keras
 import numpy as np
 import pandas as pd
+
+%matplotlib inline 
+import matplotlib.pyplot as plt # side-stepping mpl backend
+import matplotlib.gridspec as gridspec # subplots
+import mpld3 as mpl
+
+#Import models from scikit learn module:
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import KFold   #For K-fold cross validation
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn import metrics
+
+df =pd.read_csv('/home/ellenfel/Desktop/repos/Machine-Learning-Project/Data/data.csv'
+                ,header= 0)
+
+
+
+#Clean and prepare data¶
+df.drop('id',axis=1,inplace=True)
+df.drop('Unnamed: 32',axis=1,inplace=True)
+# size of the dataframe
+len(df)
+
+df.diagnosis.unique()
+
+df['diagnosis'] = df['diagnosis'].map({'M':1,'B':0})
+df.head()
+
+#Explore Data
+#df.describe()
+
+df.describe()
+plt.hist(df['diagnosis'])
+plt.title('Diagnosis (M=1 , B=0)')
+plt.show()
+
+#nucleus features vs diagnosis¶
+features_mean=list(df.columns[1:11])
+# split dataframe into two based on diagnosis
+dfM=df[df['diagnosis'] ==1]
+dfB=df[df['diagnosis'] ==0]
+
+
+
+#Stack the data
+plt.rcParams.update({'font.size': 8})
+fig, axes = plt.subplots(nrows=5, ncols=2, figsize=(8,10))
+axes = axes.ravel()
+for idx,ax in enumerate(axes):
+    ax.figure
+    binwidth= (max(df[features_mean[idx]]) - min(df[features_mean[idx]]))/50
+    ax.hist([dfM[features_mean[idx]],dfB[features_mean[idx]]], bins=np.arange(min(df[features_mean[idx]]), max(df[features_mean[idx]]) + binwidth, binwidth) , alpha=0.5,stacked=True, normed = True, label=['M','B'],color=['r','g'])
+    ax.legend(loc='upper right')
+    ax.set_title(features_mean[idx])
+plt.tight_layout()
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
