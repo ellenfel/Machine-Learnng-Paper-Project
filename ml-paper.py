0f3eb57816647fdb84151f1c073ed21e2a0d1ebb
ellenfel <ellfelfen@gmail.com>
2022-12-18 18:38:31 +0300

    start getting results

--- ml-paper.py
+++ ml-paper.py
@@ -39,7 +39,7 @@ df['diagnosis'] = df['diagnosis'].map({'M':1,'B':0})
 df.head()
 
 #Explore Data
-#df.describe()
+df.describe()
 
 df.describe()
 plt.hist(df['diagnosis'])
@@ -70,7 +70,102 @@ plt.show()
 #Creating a test set and a training set
 traindf, testdf = train_test_split(df, test_size = 0.3)
 
-#Model Classification
+
+################################################
+################################################
+
+### Randdom Forest ###
+
+X = df.loc[:, df.columns != 'diagnosis']
+y = df.iloc[:,:1]
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+from sklearn.ensemble import RandomForestClassifier
+rf_clf = RandomForestClassifier(criterion='entropy')   
+rf_clf.fit(X_train,y_train)
+
+y_pred = rf_clf.predict(X_test)
+
+from sklearn.metrics import accuracy_score,classification_report,confusion_matrix
+accuracy_score(y_test,y_pred)
+
+
+################################################
+################################################
+
+
+def evaluate_model(model, X_test, y_test):
+    from sklearn import metrics
+
+    # Predict Test Data 
+    y_pred = model.predict(X_test)
+
+    # Calculate accuracy, precision, recall, f1-score, and kappa score
+    acc = metrics.accuracy_score(y_test, y_pred)
+    prec = metrics.precision_score(y_test, y_pred)
+    rec = metrics.recall_score(y_test, y_pred)
+    f1 = metrics.f1_score(y_test, y_pred)
+    kappa = metrics.cohen_kappa_score(y_test, y_pred)
+
+    # Calculate area under curve (AUC)
+    y_pred_proba = model.predict_proba(X_test)[::,1]
+    fpr, tpr, _ = metrics.roc_curve(y_test, y_pred_proba)
+    auc = metrics.roc_auc_score(y_test, y_pred_proba)
+
+    # Display confussion matrix
+    cm = metrics.confusion_matrix(y_test, y_pred)
+
+    return {'acc': acc, 'prec': prec, 'rec': rec, 'f1': f1, 'kappa': kappa, 
+            'fpr': fpr, 'tpr': tpr, 'auc': auc, 'cm': cm}
+
+from sklearn import tree
+
+# Building Decision Tree model 
+dtc = tree.DecisionTreeClassifier(random_state=0)
+dtc.fit(X_train, y_train)
+
+# Evaluate Model
+dtc_eval = evaluate_model(dtc, X_test, y_test)
+
+# Print result
+print('Accuracy:', dtc_eval['acc'])
+print('Precision:', dtc_eval['prec'])
+print('Recall:', dtc_eval['rec'])
+print('F1 Score:', dtc_eval['f1'])
+print('Cohens Kappa Score:', dtc_eval['kappa'])
+print('Area Under Curve:', dtc_eval['auc'])
+print('Confusion Matrix:\n', dtc_eval['cm'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#  kf = KFold(5,shuffle=False) #bug
+
 #Generic function for making a classification model and accessing the performance. 
 # From AnalyticsVidhya tutorial
 def classification_model(model, data, predictors, outcome):
@@ -85,7 +180,7 @@ def classification_model(model, data, predictors, outcome):
   print("Accuracy : %s" % "{0:.3%}".format(accuracy))
 
   #Perform k-fold cross-validation with 5 folds
-  kf = KFold(5,shuffle=False) #bug
+  kf = KFold(n_splits=5,  shuffle=False, random_state=None)
   error = []
   for train, test in kf:
     # Filter training data
@@ -106,13 +201,31 @@ def classification_model(model, data, predictors, outcome):
   model.fit(data[predictors],data[outcome]) 
   
   
-  
 
 #Logistic Regression model
 predictor_var = ['radius_mean','perimeter_mean','area_mean','compactness_mean','concave points_mean']
 outcome_var='diagnosis'
 model=LogisticRegression()
 classification_model(model,traindf,predictor_var,outcome_var)
+
+predictor_var = ['radius_mean']
+model=LogisticRegression()
+classification_model(model,traindf,predictor_var,outcome_var)
+
+
+#Decision Tree Model
+predictor_var = ['radius_mean','perimeter_mean','area_mean','compactness_mean','concave points_mean']
+model = DecisionTreeClassifier()
+classification_model(model,traindf,predictor_var,outcome_var)
+
+
+#Random Forest
+# Use all the features of the nucleus
+predictor_var = features_mean
+model = RandomForestClassifier(n_estimators=100,min_samples_split=25, max_depth=7, max_features=2)
+classification_model(model, traindf,predictor_var,outcome_var)
+
+
 
 
 
